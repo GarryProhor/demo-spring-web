c701318a808139baf5a0f44fac9280adca8e7740
GarryProhor <igorprohorchenko@gmail.com>
2024-03-23 22:22:50 -0800

    feat: implements inMemory

--- manager-app/src/main/java/com/example/managerapp/controller/ProductsController.java
+++ manager-app/src/main/java/com/example/managerapp/controller/ProductsController.java
@@ -3,10 +3,24 @@ package com.example.managerapp.controller;
 import com.example.managerapp.service.ProductService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import java.util.stream.IntStream;
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("catalog/products")
 public class ProductsController {
 
     private final ProductService productService;
+
+
+
+    @RequestMapping(value = "list", method = RequestMethod.GET)
+    public String getProductsList(Model model){
+        model.addAttribute("products", this.productService.findAllProducts());
+        return  "catalog/products/list";
+    }
 }
--- manager-app/src/main/java/com/example/managerapp/service/DefaultProductService.java
+++ manager-app/src/main/java/com/example/managerapp/service/DefaultProductService.java
@@ -1,12 +1,20 @@
 package com.example.managerapp.service;
 
+import com.example.managerapp.entity.Product;
 import com.example.managerapp.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class DefaultProductService implements ProductService{
 
     private final ProductRepository productRepository;
+
+    @Override
+    public List<Product> findAllProducts() {
+        return this.productRepository.findAll();
+    }
 }
